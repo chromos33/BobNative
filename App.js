40da6296e394da900423de8927c6e7ef82af5c65
Benjamin Kreft <benjamin@kreft.com>
2020-05-23 18:14:52 +0200

    init

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import FontAwesome, { SolidIcons, RegularIcons, BrandIcons } from 'react-native-fontawesome';
+import { loadAsync } from 'expo-font';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    loadAsync({
+      // Load a font `Montserrat` from a static resource
+      "fa-solid-900": require('./assets/fonts/fa-solid-900.ttf')
+    });
+  }
+  getStyles()
+  {
+    return StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    });
+  }
+  handleClick(e)
+  {
+    Alert-alert("Titel254","Test25");
+  }
+  render(){
+    return (
+      <View style={this.getStyles().container}>
+        <Text onPress={this.handleClick}>Start point App.js CUCK</Text>
+        <FontAwesome icon={SolidIcons.child} />
+      </View>
+    );
+  }
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
